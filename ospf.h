b482e36ea57cdacec369765272aa9fd8e85126f5
João Pedro Chagas <jpchagas@gmail.com>
2017-11-13 10:27:12 -0200

    Mudanças a serem submetidas: 	new file:   helper/roteador 	new file:   helper/tf_redes2_modify.c 	modified:   ospf.h

--- ospf.h
+++ ospf.h
@@ -12,4 +12,22 @@ struct OSPF_HEADER {
     u_int8_t authdata[OSPF_AUTH_SIZE];
 }__attribute__((__packed__));
 
+struct HELLO_HEADER {
+    struct in_addr mask;
+    u_int16_t helloint;
+    u_int8_t options;
+    u_int8_t priority;
+    u_int32_t deadint;
+    struct in_addr dr;
+    struct in_addr bdr;
+    struct in_addr neighbor[1];
+} __attribute__((__packed__));
+
+struct DBD_HEADER {
+	u_int16_t mtu;
+	u_int8_t options;
+	u_int8_t dbdescript;
+	u_int32_t ddsequence;
+}__attribute__((__packed__));
+
 #endif
