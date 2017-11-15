900550427ec9e3b849a5833ae8f66aeffba5a485
João Pedro Chagas <jpchagas@gmail.com>
2017-11-15 13:36:56 -0200

    attack base and ospf.h

--- ospf.h
+++ ospf.h
@@ -10,7 +10,7 @@ struct OSPF_HEADER {
     u_int16_t chksum;
     u_int16_t authtype;
     u_int8_t authdata[OSPF_AUTH_SIZE];
-}__attribute__((__packed__));
+};
 
 struct HELLO_HEADER {
     struct in_addr mask;
@@ -21,13 +21,24 @@ struct HELLO_HEADER {
     struct in_addr dr;
     struct in_addr bdr;
     struct in_addr neighbor[1];
-} __attribute__((__packed__));
+};
 
 struct DBD_HEADER {
 	u_int16_t mtu;
 	u_int8_t options;
 	u_int8_t dbdescript;
 	u_int32_t ddsequence;
+};
+
+struct LSA_HEADER_ACK{
+	u_int16_t age;
+    u_int8_t options;
+    u_int8_t type_lsa;
+    struct in_addr link_state_id;
+    struct in_addr adv_router;
+    u_int32_t seq;
+    u_int16_t chksum;
+    u_int16_t len;
 }__attribute__((__packed__));
 
 #endif
